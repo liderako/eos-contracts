6ca41500e2345dc365ab69124a8bbcd363e19a53
asvirido <trotillostudio@gmail.com>
2018-06-11 12:45:12 +0300

    update python scripts

--- python_scripts/getInfoChainRequests.py
+++ python_scripts/getInfoChainRequests.py
@@ -1,8 +1,23 @@
 import requests
 import json
+import sys
 
-r = requests.get('http://127.0.0.1:8888/v1/chain/get_info')
+def 	printJsonInfo(info):
+	for x in info:
+		print (x, info[x])
 
-info = json.loads(r.text)
-for x in info:
-	print (x, "", info[x])
+def 	getInfo():
+	r = requests.get('http://127.0.0.1:8888/v1/chain/get_info')
+
+	info = json.loads(r.text)
+	return (info)
+
+def 	getBlock(id):
+
+	data = '{"block_num_or_id":' + str(id) + "}"
+	r = requests.post('http://127.0.0.1:8888/v1/chain/get_block', data=data)
+	info = json.loads(r.text)
+	# printJsonInfo(info)
+	return (info)
+
+# getBlock(sys.argv[1])
