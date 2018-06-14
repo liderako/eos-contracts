8ae3d66f22dcf026af2ad6597336cd7bae022f08
liderako <trotillostudio@gmail.com>
2018-06-14 10:33:13 +0300

    Update getInfoChainRequests.py

--- python_scripts/getInfoChainRequests.py
+++ python_scripts/getInfoChainRequests.py
@@ -11,12 +11,10 @@ def 	printJsonInfo(info):
 
 def 	getInfo():
 	r = requests.get('http://127.0.0.1:8888/v1/chain/get_info')
-
 	info = json.loads(r.text)
 	return (info)
 
 def 	getBlock(id):
-
 	data = '{"block_num_or_id":' + str(id) + "}"
 	r = requests.post('http://127.0.0.1:8888/v1/chain/get_block', data=data)
 	info = json.loads(r.text)
