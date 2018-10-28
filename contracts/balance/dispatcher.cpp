2d1591a6fa1a1cc51dd9c32d245a79361ffa580b
Anton Sviridov <asvirido@e1r7p5.unit.ua>
2018-10-28 17:24:37 +0200

    fix bug

--- contracts/balance/dispatcher.cpp
+++ contracts/balance/dispatcher.cpp
@@ -3,7 +3,7 @@
 extern "C" {
 using namespace balancebook;
 	void 	apply( eosio::name self, eosio::name code, eosio::name action ) {
-		if (code == N(eosio.token) && action == eosio::name("transfer") ) {
+		if (code == eosio::name("eosio.token") && action == eosio::name("transfer") ) {
 			execute_action( self, code, &balance::deposit );
 		}
 		else if ( code == self && action == eosio::name("withdraw") ) {
